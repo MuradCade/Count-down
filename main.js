c454a6685cfae093b23e4fe515902f06b9f3e55c
Murad Cade <60525999+MuradCade@users.noreply.github.com>
2021-05-30 18:32:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+const countdown = () => {
+    const countdate = new Date('june 29,  2021 00:00:00').getTime();
+    const now = new Date().getTime();
+    const gap = countdate - now;
+
+    //How The Fuck Does Time Work?
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    //Calculate the shit
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+
+    document.querySelector(".day").innerText = textDay;
+    document.querySelector(".hour").innerText = textHour;
+    document.querySelector(".minute").innerText = textMinute;
+    document.querySelector(".second").innerText = textSecond;
+
+
+}
+
+
+setInterval(countdown, 1000);
